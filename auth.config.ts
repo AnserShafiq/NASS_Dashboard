8232f9f631c0fa231ce7a0f023b5c9a860f2bb34
AnserShafiq <ansershafiq@gmail.com>
2025-01-29 00:16:58 +0500

    Trying to overcome Auth secret error

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,7 @@
 import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig: NextAuthConfig = {
-  secret: 'S9/t/JdYQPFZm9vuhVu58gwxAaGGx7NO9SO8w+H9yUw=',
+  secret: process.env.SESSION_SECRET,
   pages: {
     signIn: '/login',
     signOut: '/'
